fe8a85302ce8a2678f87f41a2a1a50383f3d1bfe
Aleksey Berezka <a.berezka@dodopizza.com>
2022-07-28 12:21:28 +0300

    Added Project.swift

---
+++ Project.swift
@@ -0,0 +1,13 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+// swiftformat:disable all
+let project = Project.externalDependency(
+  name: "NInject",
+  packages: [
+  ],
+  dependencies: [
+  ],
+  testHelpersFolder: "TestHelpers",
+  testsFolder: "Tests"
+)
