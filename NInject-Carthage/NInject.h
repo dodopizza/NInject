8609f75e449263bc805873b3c189d8113104c6a0
Aleksey Berezka <a.berezka@dodopizza.com>
2022-10-06 14:24:06 +0600

    Added Carthage Support

---
+++ NInject-Carthage/NInject.h
@@ -0,0 +1,18 @@
+//
+//  NInject.h
+//  NInject
+//
+//  Created by Aleksey Berezka on 06.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NInject.
+FOUNDATION_EXPORT double NInjectVersionNumber;
+
+//! Project version string for NInject.
+FOUNDATION_EXPORT const unsigned char NInjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NInject/PublicHeader.h>
+
+
